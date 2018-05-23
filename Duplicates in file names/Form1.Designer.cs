faa31d6380fc8f09f4c655d7923cbc6642e31b4d
absolitudo <absolitudo@gmail.com>
2018-05-23 18:21:05 +0200

    update gitignore, window size adjustments

--- Duplicates in file names/Form1.Designer.cs
+++ Duplicates in file names/Form1.Designer.cs
@@ -34,9 +34,13 @@ namespace Duplicates_in_file_names
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(740, 400);
+            this.ClientSize = new System.Drawing.Size(826, 479);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(842, 517);
+            this.MinimumSize = new System.Drawing.Size(842, 517);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Duplicates in file names";
             this.ResumeLayout(false);
 
         }
