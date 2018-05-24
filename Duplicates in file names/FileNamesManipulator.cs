a42bfd569c3e63992c31807ff2c802bea124e970
absolitudo <absolitudo@gmail.com>
2018-05-24 16:57:41 +0200

    get file names from paths

--- Duplicates in file names/FileNamesManipulator.cs
+++ Duplicates in file names/FileNamesManipulator.cs
@@ -1,9 +1,15 @@
-﻿public class FileNamesManipulator
+﻿using System.IO;
+
+public class FileNamesManipulator
 {
     private string[] fileNames;
 
     public void SetFileNames(string[] names)
     {
+        for(int index = 0; index < names.Length; index += 1)
+        {
+            names[index] = Path.GetFileName(names[index]);
+        }
         fileNames = names;
     }
 
